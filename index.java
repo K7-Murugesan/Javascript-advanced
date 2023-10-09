0879460f473cac1f9ebd2ff75f880d020f1a4d0a
K7-Murugesan <141261540+K7-Murugesan@users.noreply.github.com>
2023-10-09 19:25:04 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,8 @@
+class index{
+    public static void main(String[] args) {
+        if (System.out.append("Welcome")==null) {
+            System.out.println("Hello");
+        } else {
+            System.out.println("World");
+        }
+}}
